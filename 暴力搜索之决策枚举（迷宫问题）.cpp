5c37bab29987be781c747893a289b8fc2f9c1d7f
wait1210day <38128413+wait1210day@users.noreply.github.com>
2019-08-16 16:24:41 +0800

    Add files via upload

--- 暴力搜索之决策枚举（迷宫问题）.cpp
+++ 暴力搜索之决策枚举（迷宫问题）.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 using namespace std;
 
+/* 队列中的可用方向 */
 #define UP		1
 #define DOWN	2
 #define LEFT	3
@@ -18,21 +19,27 @@ struct Addr
 };
 vector<Addr> tred;
 
+/* 输出一个决策 */
 void dump_selection(int selection)
 {
+	if (tred.empty())
+		cout << "Backtrace: ";
+	else
+		cout << " -> ";
+	
 	switch (selection)
 	{
 		case UP:
-			cout << "-> UP ";
+			cout << "UP";
 			break;
 		case DOWN:
-			cout << "-> DOWN ";
+			cout << "DOWN ";
 			break;
 		case LEFT:
-			cout << "-> LEFT ";
+			cout << "LEFT ";
 			break;
 		case RIGHT:
-			cout << "-> RIGHT ";
+			cout << "RIGHT ";
 			break;
 	}
 }
@@ -48,6 +55,7 @@ bool array_edge_protector(int plen, int len, int x, int y)
 		return true;
 }
 
+/* 路径死循环保护（依赖全局变量 tred），或称 DFS 访问标记 */ 
 bool deadloop_protector(int x, int y)
 {
 	Addr add;
@@ -63,13 +71,15 @@ bool deadloop_protector(int x, int y)
 }
 
 bool resolver(int *data[], int plen, int len, int ax, int ay, int bx, int by, int lastsec)
-{	
+{
 	if (ax == bx && ay == by) return true;
 	int lastax = ax, lastay = ay;
 	
 	Addr add;
-
+	
+	/* 创建决策队列，每次循环都尝试队首的决策，然后使其出队 */
 	queue<int> ql;
+	/* 基于方向决策的死循环保护，同时可以减少决策尝试的次数 */
 	if (lastsec != DOWN) ql.push(UP);
 	if (lastsec != UP) ql.push(DOWN);
 	if (lastsec != RIGHT) ql.push(LEFT);
@@ -77,6 +87,7 @@ bool resolver(int *data[], int plen, int len, int ax, int ay, int bx, int by, in
 	
 	while (!ql.empty())
 	{
+		/* 按照队列执行决策，并不是所有case都会执行 */
 		switch (ql.front())
 		{
 			case UP:
@@ -92,19 +103,25 @@ bool resolver(int *data[], int plen, int len, int ax, int ay, int bx, int by, in
 				if (array_edge_protector(plen, len, ax, ay)) ay++;
 				break;
 		}
+		
+		/* 数组边界保护，触发 array_edge_protector 表示已越出迷宫 */
 		if (!array_edge_protector(plen, len, ax, ay))
 		{
+			/* 回滚决策并重新开始循环 */
   			ql.pop();
 			ax = lastax;
 			ay = lastay;
 			continue;
 		}
 
+		/* DFS 标记测试（路径死循环保护） */
 		if (deadloop_protector(ax, ay))
 			return false;
 		
+		/* 当前决策是否正确 */
 		if (data[ax][ay] == PASS)
 		{
+			/* 继续以当前位置搜索 */
 			dump_selection(ql.front());
 			if (resolver(data, plen, len, ax, ay, bx, by, ql.front()))
 				return true;
@@ -121,8 +138,11 @@ bool resolver(int *data[], int plen, int len, int ax, int ay, int bx, int by, in
 		lastay = ay;
 		add.x = ax;
 		add.y = ay;
+		/* 标记本次搜寻到的位置 */
 		tred.push_back(add);
 	}
+	
+	/* 所有可能的决策已枚举完，但是没有成功的尝试 */ 
 	return false;
 }
 
@@ -151,7 +171,14 @@ int main()
 		
 		cin >> startx >> starty >> tgx >> tgy;
 	
-		cout << endl << "Resolver: " << resolver(data, n, n, startx, starty, tgx, tgy, -1) << endl;
+		if (resolver(data, n, n, startx, starty, tgx, tgy, -1))
+		{
+			cout << "YES" << endl;
+		}
+		else
+		{
+			cout << "NO" << endl;
+		}
 		tred.clear();
 	}
 	return 0;
